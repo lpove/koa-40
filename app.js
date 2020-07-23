82af617002023858290ebbab0ffe505705c7d987
leipan <leipan@xueqiu.com>
2020-07-23 14:59:20 +0800

    feat: use context

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const http = require('http');
 
 class Application {
-    constructor () {
+    constructor() {
         this.middleware = null;
     }
 
-    listen (...arg) {
-        const server = http.createServer(this.middleware);
-        server.listen(...arg);
+    use(middleware) {
+        this.middleware = middleware;
     }
 
-    use (middleware) {
-        this.middleware = middleware;
+    listen(...arg) {
+        const server = http.createServer((req, res) => {
+            // 构造 Context 对象
+            const ctx = new Context(req, res);
+
+            // 此时处理为与 koa 兼容 Context 的 app.use 函数
+            this.middleware(ctx);
+
+            // ctx.body 为响应内容
+            ctx.res.end(ctx.body);
+        });
+        server.listen(...arg);
+    }
+}
+
+// 构造 Context 类
+class Context {
+    constructor(req, res) {
+        this.req = req;
+        this.res = res;
     }
 }
 
--- demo.js
+++ demo.js
@@ -1,14 +1,15 @@
 const open = require('open');
-const Application = require('./app')
+const Application = require('./app');
 
-const app = new Application(); 
+const app = new Application();
 
-app.use((req, res) =>{
-    res.end('hello world')
-})
+app.use(ctx => {
+    console.log('ctx:', ctx);
+    ctx.body = 'hello, world'
+});
 
-app.listen(3000, ()=>{
-    open("http://localhost:3000/", "chrome");
+app.listen(3000, () => {
+    open('http://localhost:3000/', 'chrome');
 
-    console.log('listen 3000 port!')
+    console.log('listen 3000 port!');
 });
